ecdca668039cfccb4db286dc606eb8d71849f1bf
Olle Kaiser <yogsther@gmail.com>
2019-02-22 17:37:47 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-## Coming soon!
+# Java Crypt
+
+Java Crypt kan kryptera eller dekryptera ett textmeddelande sparat i en fil med hjälp av en nyckel, också sparad i en fil.
+
+## Innehållsförteckning
+1. [Syfte](#Syfte)
+2. [Koden](#Koden)
+3. [X-OR-CAST](#X-OR-CAST)
+4. [För att köra](#För)
+
+
+## Syfte
+Skapa ett program som kan kryptera och dekryptera en fil med hjälp av en nyckel.
+
+## Koden
+
+![Simple Steps](img/simple_steps.PNG)
+
+1. Ladda in meddelandet och nyckeln, se till att nyckeln är lika lång eller längre än meddelandet.
+2. Konvertera begge till byte arrays.
+3. x-or-casta nyckeln över meddelandet och skapa 
+produkten
+4. Skriv produkten till output
+
+### X-OR-CAST
+
+Metoden encryt() tar in meddelandet och nyckeln. Efter den koverterat om dem till byte arrays loopas dem igenom och för varje byte körs en x-or-cast med meddelandet och nyckeln. 
+```java
+(byte) (message[i] ^ key[i]);
+````
+
+## För att köra
+Gå till [Releases](https://github.com/Yogsther/javaCrypt/releases) och 
+ladda ned den senaste .JAR filen, eller bygg ```Crypt.java``` som finns i ```/src```
+
+### Kör
+```
+$ java Crypt message_dir key_dir [output_dir]
+```
+
+Standard output är ```output.txt```
+
+
--- src/Crypt.java
+++ src/Crypt.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Crypt {
-    // ARGS: 0: File path, 1: In binary (bool, 0-1)
     public static String textToCopy;
 
     /** Extends a string
@@ -12,7 +11,7 @@ public class Crypt {
      * @return The new, extended key
      */
     public static String checkKey(String key, int length) {
-        while (key.length() < length) key += key;
+        while (key.length() < length) key += key; // While key is too short, extend it by dublicating it
         return key;
     }
 
@@ -22,17 +21,21 @@ public class Crypt {
      * @return The encrypted or decrypted string
      */
     public static String encrypt(String message, String key) {
-        if (key.length() < message.length()) key = checkKey(key, message.length());
+        if (key.length() < message.length()) key = checkKey(key, message.length()); // Make sure key is long enough
 
-        byte[] message_arr = message.getBytes();
+        // Convert message and key to byte arrays
+        byte[] message_arr = message.getBytes(); 
         byte[] key_arr = key.getBytes();
 
+        // Create a new byte array to store the final message / encrypted code
         byte[] encrypted = new byte[message_arr.length];
 
+        // Loop through the message and encrypt it with the key
         for (int i = 0; i < message_arr.length; i++) {
             encrypted[i] = (byte)(message_arr[i] ^ key_arr[i]);
         }
-
+        
+        // Return the final array convertet to a String
         return new String(encrypted);
     }
 
@@ -41,44 +44,40 @@ public class Crypt {
      * @return The read file
      */
     public static String readFile(String location) {
-        String readString;
+        String readString; // String to hold the loaded data
         try {
-            Scanner scanner = new Scanner(new File(location), "UTF-8");
-            readString = scanner.useDelimiter("\\A").next();
+            Scanner scanner = new Scanner(new File(location), "UTF-8"); // Create a scanner and load the file
+            readString = scanner.useDelimiter("\\A").next(); // Read through
             scanner.close();
         } catch (FileNotFoundException e) {
+            // Watch for exception
             System.out.println("File not found.");
             return "";
         }
-        return readString;
+        return readString; // Read file to string
     }
 
     public static void main(String[] args) {
-        if (args.length > 1) {
-            String product = encrypt(readFile(args[0]), readFile(args[1]));
+        if (args.length > 1) { // Make sure args are provided
+            String product = encrypt(readFile(args[0]), readFile(args[1])); // Encrypt or decrypt input message
 
             Writer writer = null;
             String output = "output.txt";
-            if(args.length > 2) output = args[2]; 
+            if(args.length > 2) output = args[2]; // If a third argument is provided, use that as output. 
 
             try {
                 writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(output), "utf-8"));
-                writer.write(product);
-                System.out.println("Success!");
-            } catch (IOException ex) {
-                // Report
-            } finally {
+                new FileOutputStream(output), "utf-8")); // Create file from output
+                writer.write(product); // Write encrypted or decrypted message
+                System.out.println("Success!"); // Signal when success
+            } catch (IOException ex) {} finally {
                 try {
                     writer.close();
-                } catch (Exception ex) {
-                    /*ignore*/ }
+                } catch (Exception ex) {}
             }
-
-
         } else {
-            // Ask in terminal
-            System.out.println("At least two arguments are needed, Directory of message and location");
+            // TODO: Ask for inputs in terminal
+            System.out.println("Usage: java Crypt message_dir key_dir [output_dir]");
             return;
         }
     }
